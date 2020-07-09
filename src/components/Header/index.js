a5cfdaa76545e6e71716f341e65a635f5d133f85
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-07-09 23:54:03 +0700

    update header style

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './style.css'
 import PropTypes from 'prop-types'
+import { ClockCircleOutlined } from '@ant-design/icons'
 
 export function Header({title = 'Title'}) {
   return (
     <header>
-      <h1>{title}</h1>
+      <h1>{title} <ClockCircleOutlined spin={true} /> </h1>
     </header>
   )
 }
