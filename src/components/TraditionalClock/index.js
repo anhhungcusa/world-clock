9552a1fbef25a6f7cd5b7603c63b890e0167bc94
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-07-09 23:06:03 +0700

    update TraditionalClock

--- src/components/TraditionalClock/index.js
+++ src/components/TraditionalClock/index.js
@@ -1,26 +1,14 @@
 import React, { useState, useEffect, useMemo, useRef, useCallback } from 'react'
 import PropTypes from 'prop-types'
-import moment from 'moment-timezone'
 import './style.css'
 import { DrawLockUtil } from '../../utils'
 export function TraditionalClock({
-  zone,
+  time,
   size = 200,
   timeFormat = 'standard',
   hourFormat = 'standard',
 }) {
-  const [time, setTime] = useState(moment().tz(zone))
   const [radius, setRadius] = useState(size / 2)
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(time => {
-        return time.clone().add(1, 's')
-      })
-    }, 1000)
-    return () => {
-      clearInterval(interval)
-    }
-  }, [])
   const [drawingContext, setDrawingContext] = useState(null)
   const clockCanvasRef = useRef()
 
@@ -38,24 +26,21 @@ export function TraditionalClock({
   }, [drawingContext, radius, size])
 
   useEffect(() => {
-    if (drawingContext === null) return
+    if (drawingContext === null || time === null) return
     const ctx = drawingContext
     DrawLockUtil.drawFace(ctx, radius)
     DrawLockUtil.drawTicks(ctx, radius)
     const hours = time.get('hours')
     const minute = time.get('minutes')
     const seconds = time.get('seconds')
-    const meridiem = hours >= 12 ? 'PM' : 'AM' 
+    const meridiem = hours >= 12 ? 'PM' : 'AM'
     DrawLockUtil.drawNumbers(ctx, radius, isDrawRoman, isDraw24hour, meridiem)
     DrawLockUtil.drawTime(ctx, radius, hours, minute, seconds)
   }, [time, drawingContext, radius, isDrawRoman, isDraw24hour])
 
   return (
-    <div className="d-flex flex-column justify-content-center align-item-center ">
-      <div className="traditional-clock" style={{ width: String(size) + 'px' }}>
-        <canvas width={size} height={size} ref={clockCanvasRef} />
-      </div>
-      <div> {time.format('hh:mm:ss - A')} </div>
+    <div className="traditional-clock d-flex justify-content-center align-item-center">
+      <canvas width={size} height={size} ref={clockCanvasRef} />
     </div>
   )
 }
@@ -63,7 +48,7 @@ export function TraditionalClock({
 export const TraditionalClockMemorized = React.memo(TraditionalClock)
 
 TraditionalClock.propTypes = {
-  zone: PropTypes.string.isRequired,
+  time: PropTypes.object.isRequired,
   size: PropTypes.number.isRequired,
   timeFormat: PropTypes.oneOf(['standard', '24h']).isRequired,
   hourFormat: PropTypes.oneOf(['roman', 'standard']).isRequired,
